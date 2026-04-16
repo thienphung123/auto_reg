8eb23c86eeb092d0045894e6a5b68135ba33b8be
thienphung123 <jointhien9x@gmail.com>
2026-04-17 06:53:55 +0700

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 set -eu
 
+# 1. KHỞI TẠO MÀN HÌNH ẢO (Xvfb) - Bước sống còn để chạy trên Cloud
+# Tạo màn hình tàng hình số :99 để trình duyệt ảo có chỗ hoạt động
+Xvfb :99 -screen 0 1024x768x24 &
+export DISPLAY=:99
+
+# 2. GIỮ NGUYÊN PHẦN QUẢN LÝ THƯ MỤC CỦA TÁC GIẢ
 APP_DIR="/app"
 RUNTIME_DIR="${APP_RUNTIME_DIR:-/runtime}"
 
@@ -17,4 +23,5 @@ ln -sfn "${RUNTIME_DIR}/smstome_all_numbers.txt" "${APP_DIR}/smstome_all_numbers
 ln -sfn "${RUNTIME_DIR}/smstome_uk_deep_numbers.txt" "${APP_DIR}/smstome_uk_deep_numbers.txt"
 ln -sfn "${RUNTIME_DIR}/logs/solver.log" "${APP_DIR}/services/turnstile_solver/solver.log"
 
+# 3. CHẠY ỨNG DỤNG CHÍNH
 exec python main.py
